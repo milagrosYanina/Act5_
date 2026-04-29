932cb4fb8903a5f2cb80e5fa02e99b1823dc1a6b
YaninamiLagros <mily.ostay@gmail.com>
2026-04-28 22:52:20 -0300

    Agregando las escenas al grupo "Blocks"

--- señales-act-5/block_violet.gd
+++ señales-act-5/block_violet.gd
@@ -1,11 +1,22 @@
 extends Sprite2D
+
 var EscenaV= preload("res://VioletGenerator.tscn")
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass# Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
 func Generate_BlockV():
 	var generar= EscenaV.instantiate()
 	add_child(generar)
+
+func Delete():
+	Generate_BlockV().queue_free()
+	
 func _on_button_button_down() -> void:
 	Generate_BlockV() # Replace with function body.
--- señales-act-5/señales-act-5/block_red.gd
+++ señales-act-5/señales-act-5/block_red.gd
@@ -1,13 +1,25 @@
 extends Sprite2D
+
 var escena = preload("res://RedGenerator.tscn")
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
 func Generate_BlockR():
 	var generar= escena.instantiate()
 	add_child(generar)
 	
+func DeleteR():
+	Generate_BlockR().queue_free()
+	
+
+
 func _on_button_button_down() -> void:
 	Generate_BlockR()
 	 # Replace with function body.
